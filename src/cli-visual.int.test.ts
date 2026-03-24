2497246b138d580ef8ff5fa71320116991d6c197
Christoffer Niska <christofferniska@gmail.com>
2026-03-24 18:34:02 +0200

    chore: release v0.9.0

--- src/cli-visual.int.test.ts
+++ src/cli-visual.int.test.ts
@@ -97,7 +97,7 @@ describe("cli visual regression", () => {
     const out = await runCliPlain(["--help"]);
     expect(out).toBe(
       dedent(`
-      Acolyte v0.8.0
+      Acolyte v0.9.0
 
       Usage
         acolyte
